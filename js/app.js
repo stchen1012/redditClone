ed503c688d0c6af7349942057bfba4903b6f4384
Scott Sauceda <msauceda0812@gmail.com>
2019-09-25 15:23:49 -0400

    combined login and signup js

--- js/app.js
+++ js/app.js
@@ -42,3 +42,89 @@ function Post(postId, postTitle, postDescription, postUser) {
     this.postDescription = postDescription;
     this.postUser = postUser;
 }
+
+
+//Attempt to get the element using document.getElementById
+var loginForm = document.querySelector('.form-login');
+var signupForm = document.querySelector('.form-signup');
+ 
+//If it isn't "undefined" and it isn't "null", then it exists.
+if(typeof(loginForm) != 'undefined' && loginForm != null){
+    loginForm.addEventListener('submit', loginUser);
+    console.log('login page');
+} else if(typeof(signupForm) != 'undefined' && signupForm != null) {
+    document.querySelector('.form-signup').addEventListener('submit', signupUser);
+    console.log('signup page');
+} else {
+    console.log('none');
+}
+
+// login
+function loginUser(event){
+    event.preventDefault();
+    let logEmail = document.getElementById('inputEmail').value;
+    console.log(inputEmail);
+    let logPassword = document.getElementById('inputPassword').value;
+    console.log(inputPassword);
+
+        fetch("http://thesi.generalassemb.ly:8080/login", {
+           method: 'post',
+           headers:{
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+           body: JSON.stringify({
+               email: logEmail,
+               password: logPassword,
+           })
+       })
+           .then((response )=> {
+               return response.json();
+           })
+           .then((json) =>{
+               console.log(json);
+               alert("Sign in Successful");
+               window.location.replace("file:///Users/marcus/Documents/generalAssembly/projects/redditClone/index.html");
+           })
+           .catch(function(error){
+               console.log(error);
+               alert("User Failed To Sign");
+           })
+}
+
+//  signup
+function signupUser(event){
+    console.log('signupUser called');
+    event.preventDefault();
+    let inputEmail = document.getElementById('inputEmail').value;
+    console.log(inputEmail);
+    let inputUsername = document.getElementById('inputUsername').value;
+    console.log(inputUsername);
+    let inputPassword = document.getElementById('inputPassword').value;
+    console.log(inputPassword);
+
+        fetch("http://thesi.generalassemb.ly:8080/signup", {
+           method: 'post',
+           headers:{
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+           body: JSON.stringify({
+               email: inputEmail,
+               username: inputUsername,
+               password: inputPassword,
+           })
+       })
+           .then((response )=> {
+               return response.json();
+           })
+           .then((json) =>{
+               console.log(json);
+               alert("New User Created");
+               window.location.replace("file:///Users/marcus/Documents/generalAssembly/projects/redditClone/index.html");
+           })
+           .catch(function(error){
+               console.log(error);
+               alert("Failed To Create User");
+           })
+}
